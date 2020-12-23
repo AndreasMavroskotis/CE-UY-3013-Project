a720753edd83a4938999a8f19b64b73451a18b48
AndreasMavroskotis <73948125+AndreasMavroskotis@users.noreply.github.com>
2020-12-23 14:28:48 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-from source import TestClass
 
-t = TestClass()
-print(t)
+
+from source import version
+
+      
+def main():      
+  
+print(pd.__version__)
+print(np.__version__)
+print(sns.__version__)
+
+version=("pandas,numpy or seaborn")
+
+version=Versions("Data_for_computing.xls",version)
